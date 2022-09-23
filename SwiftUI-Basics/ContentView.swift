18fb3a871337f3e0dfb31f5f08b4a1fd103d2c6f
shamitomita <shamitomita@gmail.com>
2022-09-22 23:13:56 -0400

    sf symbol

--- SwiftUI-Basics/ContentView.swift
+++ SwiftUI-Basics/ContentView.swift
@@ -17,6 +17,16 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding(.top)
+                VStack(spacing: 8){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:180, height:180)
+                    Text("76°")
+                        .font(.system(size: 60, weight:.medium ))
+                        .foregroundColor(.white)
+                }
                 Spacer()
                    
             }
