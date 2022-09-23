875220ca1aa8490b4f64e0cf3f70dc45b525cd0a
shamitomita <shamitomita@gmail.com>
2022-09-22 22:56:33 -0400

    text

--- SwiftUI-Basics/ContentView.swift
+++ SwiftUI-Basics/ContentView.swift
@@ -11,8 +11,14 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .cyan, .teal, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
             VStack{
-                
+                Text("Tokyo, Japan")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding(.top)
+                Spacer()
+                   
             }
         }
     }
