b3a4ad00e28c818a28d11cca551881c6e240bdf9
shamitomita <shamitomita@gmail.com>
2022-09-22 22:40:26 -0400

    Initial Commit

---
+++ SwiftUI-Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI-Basics
+//
+//  Created by Shami Tomita on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
